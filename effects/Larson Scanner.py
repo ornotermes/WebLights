764cfb307a21e2743ce5bd1e9417c54740067602
Rikard Lindström <ornotermes@gmail.com>
2015-03-02 23:52:22 +0100

    Modified Larson Scanner to have better decay.

--- effects/Larson Scanner.py
+++ effects/Larson Scanner.py
@@ -20,25 +20,24 @@ def show():
 	form.slider("hue", "Hue", 0.0, 1.0, 0.01, form.get("hue"))
 	form.slider("saturation", "Saturation", 0.0, 1.0, 0.01, form.get("saturation"))
 	form.slider("value", "Value", 0.0, 1.0, 0.01, form.get("value"))
+	form.slider("decay", "Decay", 0.0, 1.0, 0.01, form.get("decay"))
 	form.finnish()
 
 def init():
 	config["hue"] = 0.0
 	config["saturation"] = 1.0
 	config["value"] = 1.0
-	
+	config["decay"] = 0.5
+
 def run():
+	values = [0.0]*strip.length
 	while True:
 		for d in [0, 1]:
 			for m in range(0, strip.length):
+				values = [config["decay"]*x for x in values]
+				v = abs( (d*strip.length) - m)-1
+				values[v] = config["value"]
 				for n in range(0, strip.length):
 					if stop: return
-					v = abs( (d*strip.length) - m)
-					if( n == v ) : strip.hsv( config["hue"], config["saturation"], config["value"] * 1.0 )
-					if( n == v-1 and v < strip.length-1) : strip.hsv( config["hue"], config["saturation"], config["value"] * 0.5 )
-					if( n == v-2 and v < strip.length-2) : strip.hsv( config["hue"], config["saturation"], config["value"] * 0.25 )
-					if( n == v-3 and v < strip.length-3) : strip.hsv( config["hue"], config["saturation"], config["value"] * 0.125 )
-					if( n == v-4 and v < strip.length-4) : strip.hsv( config["hue"], config["saturation"], config["value"] * 0.0625 )
-					else : strip.hsv( config["hue"], config["saturation"], 0.0 )
-
+					strip.hsv( config["hue"], config["saturation"], values[n] )
 				strip.show()
